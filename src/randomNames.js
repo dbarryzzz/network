557e8e5a4719a9d99c68c9d2f9a713c076f4bb28
Dan Barry <dan.barry@orbitlogic.com>
2018-05-23 16:44:28 -0400

    add more random series names

--- src/randomNames.js
+++ src/randomNames.js
@@ -1,2 +1,6 @@
-export const partA = ["Attila the ", "Bravery and ", "Charles in ", "Dog in ", "Edmonton ", "Fake ", "Groovy ", "Hipster ", "Indecent ", "Jobs & "];
-export const partB = ["Law", "Men", "North", "Opera", "Paris", "Queens", "Raid", "Sex", "Trouble", "Uniform", "Vendetta", "Xylophone", "Young", "Zoology"];
+export const partA = ["Attila the ", "Baltimore ", "Bravery and ", "Charles in ", "Dog in ","Downtown ", "Edmonton ", "Fake ",
+     "Groovy ", "Hipster ", "Indecent ", "Jobs & ", "Kingdom of ", "Lawn & ", "Meet the ", "Newly ", "Off the ", "Princes of ", "Quest for ",
+    "Raiders of the ", "Sinner's  ", "True ", "Unlikely ", "Voodoo ", "Wandering ",  "X-Ray ", "Young ", "Zest for" ];
+
+
+export const partB = ["Law", "Men", "North", "Opera", "Order", "Paris", "Queens", "Raid", "Sex", "Trouble", "Uniform", "Vendetta", "Walrus", "Xylophone", "Youth", "Zoology"];
