0496bf2b2d31cdd3e102f5a80a97986c983bcf77
Dan Barry <dan.barry@orbitlogic.com>
2018-06-12 18:21:36 -0400

    cleanup episode card

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -2,15 +2,22 @@ import React from 'react'
 
 import { Card, Icon, Label, Button, Popup } from 'semantic-ui-react'
 
+const episodeAired = (ep) => {return ep.rating != null ? true : false};
+
 const EpisodeCard = ({ episode, series, removeFunction }) => (
     <Card>
         <Card.Content>
             <Card.Header><h4>{series.name}</h4></Card.Header>
             <Card.Meta>Ep # {episode.number} of {series.episodesOrdered} </Card.Meta>
             <Card.Description>
-                <Label basic color='blue'>Quality</Label>
-                {series.stats.totalQuality}
+                {/* <Label basic color='blue'>Quality</Label>
+                {series.stats.totalQuality} */}
             </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <div>
+                {series.stats.flavors.map((f, i) => <Icon name={f.icon} color={f.color} size='large' key={i} />)}
+            </div>
         </Card.Content>
         <Card.Content extra>
             <div>
@@ -23,18 +30,17 @@ const EpisodeCard = ({ episode, series, removeFunction }) => (
                 <br />
             </div>
         </Card.Content>
-        <Card.Content extra>
-            <div>
-                {series.stats.flavors.map((f, i) => <Icon name={f.icon} color={f.color} size='large' key={i} />)}
-            </div>
-        </Card.Content>
+
         <Card.Content>
             <Popup
-                trigger={<Button basic icon='trash' color='black' onClick={() => removeFunction(episode)} />}
+                trigger={<Button basic icon='trash' color='black' 
+                    disabled={episodeAired(episode)} 
+                    onClick={() => removeFunction(episode)} />}               
                 content='Remove from week'
             />
             <Popup
-                trigger={<Button basic icon='recycle' color='green'/>}
+                trigger={<Button basic icon='recycle' color='green'
+                disabled={episodeAired(episode)} />}
                 content='Air a rerun'
             />        
             
