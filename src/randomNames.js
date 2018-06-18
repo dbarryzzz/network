c4aca281e64ea682704c65d89e90092c1cf4ca53
Dan Barry <dan.barry@orbitlogic.com>
2018-06-18 16:42:07 -0400

    update names

--- src/randomNames.js
+++ src/randomNames.js
@@ -1,12 +1,13 @@
-export const partA = ["Attila the ", "Baltimore ", "Bravery and ", "Charles in ", "Dog in ","Downtown ", "Edmonton ", "Fake ",  //8
-     "Groovy ", "Hipster ", "Indecent ", "Jobs & ", "Kingdom of ", "Lawn & ", "Meet the ", "Newly ", "Off the ", "Princes of ", "Quest for ", //11
+export const partA = 
+    ["42 Roads to ", "Attila the ", "Baltimore ", "Bravery and ", "Charles in ", "Dog in ","Downtown ", "Edmonton ", "Fake ", "Groovy ", //10
+    "Hipster ", "Indecent ", "Jobs & ", "Kingdom of ", "Lawn & ", "Meet the ", "New ", "Off the ", "Princes of ", "Quest for ", //10
     "Raiders of the ", "Sinner's  ", "True ", "Unlikely ", "Voodoo ", "Wandering ",  "X-Ray ", "Young ", "Zest for ", "1st & ",  //10
     "The ", "Story of ", "Friendship & " , "Barely ", "Adventures of ", "Fresh ", "Game of ", "Wacky ", "weird ", "Night ", //10
     "Hartford ", "Maybe ", "Before ", "After ", "Boise ", "My Uncle the ", "Knight ", "Red ", "Blue ", "White ", //10
-    "A Bitcoin to ", "Mistakenly ", "Rocket to ", "Sword of ", "Martian ", "Incredible "];  //6
+    "A Bitcoin to ", "Mistakenly ", "Rocket to ", "Sword of ", "Martian ", "Incredible ", "Star's ", "Happy ", "Sad ", "Jane "];  //10
 
 
-export const partB = ["Alexa", "Boogie", "Circus", "Details", "Frog", "Good", "Home", "Institute", "Jockey", "Kangaroo", //10
+export const partB = ["Alexa", "Boogie", "Circus", "Details", "Frog", "Good", "Home", "Ink", "Jockey", "Kangaroo", //10
 "Law", "Men", "North", "Opera", "Order", "Paris", "Queens", "Raid", "Sex", "Trouble",  //10
 "Uniform", "Vendetta", "Walrus", "Xylophone", "Youth", "Zoology", "Academy", "Boss", "Cardiology", "Dingo", //10
 "Election", "Fire", "G Thang", "Hobo", "Ice Cream", "Japan", "Krakatoa", "Lion", "Moon", "Narwhal"]; //10
